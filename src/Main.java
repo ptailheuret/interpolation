2939059556995b8594e5eaca5976b0d9bbd6e4f2
ptailheuret <patrick.tailheuret@gmail.com>
2015-03-21 18:26:40 +0100

    Ajout de l'affichage de l'image et du temps d'execution

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 /**
@@ -55,15 +58,19 @@ public class Main {
 		Interpolator interpolator = new Interpolator();
 		
 		//Creation de la liste de points
+		int width = 500;
+		int height = 500;
+		int tempMin = -5;
+		int tempMax = 40;
 		
 		ArrayList<Point> listPoints = new ArrayList<Point>();
 		
-		for(int u=0; u<200; u++){
+		for(int u=0; u<100; u++){
 			
 			Random r=new Random();
-			int a=r.nextInt(1000);
-			int b=r.nextInt(1000);
-			int c=r.nextInt(30+1);
+			int a = r.nextInt(width+1);
+			int b = r.nextInt(height+1);
+			int c = tempMin + r.nextInt((tempMax-tempMin)+1);
 					
 			Point A = new Point(a, b, null, c);
 			listPoints.add(A);
@@ -75,12 +82,12 @@ public class Main {
          BufferedWriter outStream = new BufferedWriter(outFile);
          
          outStream.write(String.valueOf(0));outStream.write(" "); 
-         outStream.write(String.valueOf(1000));outStream.write(" "); 
+         outStream.write(String.valueOf(width));outStream.write(" "); 
          outStream.write(String.valueOf(0));outStream.write(" "); 
-         outStream.write(String.valueOf(1000));outStream.write(" "); 
+         outStream.write(String.valueOf(height));outStream.write(" "); 
          outStream.write(String.valueOf(1));outStream.write(" "); 
-         outStream.write(String.valueOf(0));outStream.write(" "); 
-         outStream.write(String.valueOf(30));outStream.write(" "); 
+         outStream.write(String.valueOf(tempMin));outStream.write(" "); 
+         outStream.write(String.valueOf(tempMax));outStream.write(" "); 
          
          for (int k = 0; k < listPoints.size(); k++){
              outStream.write(String.valueOf(listPoints.get(k).getX()));
@@ -190,19 +197,30 @@ public class Main {
 	       
 			if(carac == 'V'){
 				System.out.println("Diagramme de Voronoi");
+				long start = System.currentTimeMillis();
 				interpolator.Voronoi(listPoints);
+				long duree = System.currentTimeMillis() - start;
+				System.out.println();
+				System.out.println("Execution time:" + duree/1000 + "s");
 				interpolator.setName("Voronoi");
 				boucle = true;				
-				System.out.println(boucle);
 			}
 			else if(carac == 'N'){
 				System.out.println("Cartographie");
+				long start = System.currentTimeMillis();
 				interpolator.nearestNeighbourSearch(listPoints);
+				long duree = System.currentTimeMillis() - start;
+				System.out.println();
+				System.out.println("Execution time:" + duree/1000 + "s");
 				interpolator.setName("Cartographie");
 				boucle = true;
 			}
 			else if(carac == 'M'){
 				System.out.println("Marching Squares");
+				long start = System.currentTimeMillis();
+				long duree = System.currentTimeMillis() - start;
+				System.out.println();
+				System.out.println("Execution time:" + duree/1000 + "s");
 				interpolator.setName("Marching Squares");
 				boucle = true;
 			}
@@ -263,6 +281,19 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		//Affichage de l'image dans un JFrame
+		
+		JFrame jframe = new JFrame(interpolator.getName());
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel vidpanel = new JLabel();
+        jframe.setContentPane(vidpanel);
+        jframe.setSize(ncols, nrows);
+        jframe.setVisible(true);
+        
+        ImageIcon image = new ImageIcon(img);
+        vidpanel.setIcon(image);
+        vidpanel.repaint();        
 		}
 	}
 }
